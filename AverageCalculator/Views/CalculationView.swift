2729b7bd1a399f5b87267f83f0b3a62617f5611c
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-02-08 21:03:28 -0500

    Made a static layout and added variables

--- AverageCalculator/Views/CalculationView.swift
+++ AverageCalculator/Views/CalculationView.swift
@@ -12,20 +12,135 @@ struct CalculationView: View {
     
     // MARK: Stored properties
         
+    @State var providedEnglishGrade = ""
+    @State var providedChemistryGrade = ""
+    @State var providedBiologyGrade = ""
+    @State var providedComputerScienceGrade = ""
+    @State var providedMathGrade = ""
+    @State var providedOutdoorEducationGrade = ""
+    @State var providedKinisiologyGrade = ""
+    @State var providedEconomicsGrade = ""
+
     
-        // MARK: Computed properties
+    // MARK: Computed properties
     
     var body: some View {
         NavigationView{
-            VStack {
-                Text("Calculation interface goes here, soon")
-            }
-            .navigationTitle("Share the Bill")
-        }
-    }
-}
-struct CalculationView_Previews: PreviewProvider {
-    static var previews: some View {
-        CalculationView()
-    }
-}
+            HStack {
+                
+                VStack(spacing:1){
+                    
+                    //All of the classes here
+                    
+                    HStack{
+                        Text("English")
+                            .bold()
+                            .padding()
+                       
+                        Spacer()
+                        
+                        TextField("Grade Goes here", text: <#T##Binding<String>#>)
+                            .padding()
+                        
+                    }
+                    
+                    HStack{
+                        Text("Biology")
+                            .bold()
+                            .padding()
+                       
+                        Spacer()
+                        
+                        Text("Grade Goes here")
+                            .padding()
+                        
+                    }
+                    
+                    HStack{
+                        Text("Chemistry")
+                            .bold()
+                            .padding()
+                       
+                        Spacer()
+                        
+                        Text("Grade Goes here")
+                            .padding()
+                        
+                    }
+                    
+                    HStack{
+                        Text("Computer Studies")
+                            .bold()
+                            .padding()
+                       
+                        Spacer()
+                        
+                        Text("Grade Goes here")
+                            .padding()
+                        
+                    }
+                    
+                    HStack{
+                        Text("Math")
+                            .bold()
+                            .padding()
+                       
+                        Spacer()
+                        
+                        Text("Grade Goes here")
+                            .padding()
+                        
+                    }
+                    
+                    HStack{
+                        Text("Outdoor Education")
+                            .bold()
+                            .padding()
+                       
+                        Spacer()
+                        
+                        Text("Grade Goes here")
+                            .padding()
+                        
+                    }
+                    
+                    HStack{
+                        Text("Kinesiology")
+                            .bold()
+                            .padding()
+                       
+                        Spacer()
+                        
+                        Text("Grade Goes here")
+                            .padding()
+                        
+                    }
+                    
+                    HStack{
+                        Text("Economics")
+                            .bold()
+                            .padding()
+                       
+                        Spacer()
+                        
+                        Text("Grade Goes here")
+                            .padding()
+                        
+                    }
+                    
+                    //History Button
+                    
+                    
+                                            }
+                Spacer()
+                                            
+                                        }
+                                        .navigationTitle("Share the Bill")
+                                    }
+                                }
+                            }
+                            struct CalculationView_Previews: PreviewProvider {
+                                static var previews: some View {
+                                    CalculationView()
+                                }
+                            }
