2edb20d7859eaa6e9311395984faa89f854f50b3
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-02-08 20:19:31 -0500

    Made the playground, and organized it.

---
+++ AverageCalculator/Views/AverageView.swift
@@ -0,0 +1,20 @@
+//
+//  AverageView.swift
+//  AverageCalculator
+//
+//  Created by Oliver Finlayson on 2023-02-08.
+//
+
+import SwiftUI
+
+struct AverageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AverageView_Previews: PreviewProvider {
+    static var previews: some View {
+        AverageView()
+    }
+}
