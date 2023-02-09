b4561a5dcee87b9f6cfbd7c5f7438ba646bd07c6
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-02-08 20:18:17 -0500

    Initial Commit

---
+++ AverageCalculator/AverageCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AverageCalculatorApp.swift
+//  AverageCalculator
+//
+//  Created by Oliver Finlayson on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct AverageCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
