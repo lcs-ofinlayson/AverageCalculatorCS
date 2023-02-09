ceaba56fbe8324b52509c3c70b5591d70f45f31b
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-02-08 20:42:55 -0500

    Made navigation title.

--- AverageCalculator/AverageCalculatorApp.swift
+++ AverageCalculator/AverageCalculatorApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct AverageCalculatorApp: App {
     var body: some Scene {
         WindowGroup {
-            AverageView()
+            NavigationView{
+                CalculationView()
+            }
         }
     }
 }
---
+++ AverageCalculator/Views/CalculationView.swift
@@ -0,0 +1,31 @@
+//
+//  CalculationView.swift
+//  AverageCalculator
+//
+//  Created by Oliver Finlayson on 2023-02-08.
+//
+
+import SwiftUI
+
+struct CalculationView: View {
+    
+    
+    // MARK: Stored properties
+        
+    
+        // MARK: Computed properties
+    
+    var body: some View {
+        NavigationView{
+            VStack {
+                Text("Calculation interface goes here, soon")
+            }
+            .navigationTitle("Share the Bill")
+        }
+    }
+}
+struct CalculationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculationView()
+    }
+}
